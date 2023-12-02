0adb7bc76c8fb8d85d39ec6cc62d2160378a5062
qwertyNippon <orlandohenry100@gmail.com>
2023-12-02 02:31:57 -0500

    update

---
+++ main.js
@@ -0,0 +1,7 @@
+const hamburger = document.querySelector('.hamburger');
+const navList = document.querySelector('nav ul');
+
+hamburger.addEventListener('click', () => {
+    hamburger.classList.toggle('active');
+    navList.classList.toggle('active');
+});
