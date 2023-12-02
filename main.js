b11a9eaab361d30cd7d7bfedd9715ae202816d2d
qwertyNippon <orlandohenry100@gmail.com>
2023-12-02 12:11:47 -0500

    update

--- main.js
+++ main.js
@@ -5,3 +5,11 @@ hamburger.addEventListener('click', () => {
     hamburger.classList.toggle('active');
     navList.classList.toggle('active');
 });
+
+const typed = new Typed('.multiple', {
+    strings: ['Wife', 'Mother', 'Japanese Teacher', 'Youtuber', 'Life-long Student'],
+    typespeed:100,
+    backSpeed:100,
+    backDelay:2000,
+    loop: true
+});
